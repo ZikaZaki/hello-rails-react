3508d94808526ea7715c5078b2c8cd9da922889f
zikazaki <17160061@su.edu.ye>
2023-02-04 06:00:10 +0300

    Fix linter errors

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    greeting = Message.order("RANDOM()").first.greeting
+    greeting = Message.order('RANDOM()').first.greeting
     render json: { message: greeting }
   end
 end
